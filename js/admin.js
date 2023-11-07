c876e7c758267cd5897fe2cbd92479825a5cc760
sicalo330 <132423242+sicalo330@users.noreply.github.com>
2023-11-06 20:58:50 -0500

    Añadir dataTables dinámicos

--- js/admin.js
+++ js/admin.js
@@ -1,167 +1,43 @@
 const getTable = async() => {
-    //Esto cargará el json de usuarios
-    const users = await fetch('./data/users.json');
-    const userElement = await users.json();
+    //Esto detectará la URL en el que estamos parados, va a partir en dos en referencia al = y accedemos a la posición 0
+    //Es decir si ?t=users, el split hará un [?t,users] y al acceder a la posición 1 dará como resultado users
+    let url = window.location.search.split('=')[1];
+    const data = await fetch(`./data/${url}.json`)
+    const elements = await data.json();
+    let tableDiv = document.querySelector("#tableDiv");
 
-    //Esto cargará el json de login
-    const login = await fetch('./data/login_info.json');
-    const loginElement = await login.json();
-    //Esto cargará el json del index
-    const index = await fetch('./data/indexElem.json');
-    const indexElement = await index.json();
-    //Esto cargará el json de diseño retro
-    const dis = await fetch('./data/diseñoRetro.json');
-    const disElement = await dis.json();
-    let user = document.querySelector("#tableDiv");
-
-    let userTable = `
-    <table id="tableUser">
+    //Como en nuestros json no tenemos un numero de columnas iguales, tuve que partirlo en varias partes
+    //Esto iniciará haciendo un thead
+    let dataTable = `
+    <table id="dataTable">
         <h1>Tabla de usuarios</h1>
         <thead>
-        <tr>
-            <th>UserId</th>
-            <th>User</th>
-            <th>Password</th>
-            <th>Rol</th>
-        </tr>
+    `
+    //El objetivo de esto es que se ciclen en los atributos de cada json porque repito en nuestros json no tenemos un número igual de columnas
+    dataTable +=`
+    <tr>
+    ${Object.keys(elements[0]).map(key => `<th scope="col">${key.toUpperCase()}</th>`).join('')};
+    </tr>
+    `
+    //Aquí pongo el thead de cierre y pongo las celdas de cada atributo
+    dataTable += `
     </thead>
     <tbody>
-    `
-
-    userElement.forEach(element => {
-        userTable += `
+    ${elements.map( row => `
         <tr>
-            <td>${element.userId}</td>
-            <td>${element.user}</td>
-            <td>${element.password}</td>
-            <td>${element.rol}</td>
+            ${Object.keys(row).map( cell => `<td>${row[cell]}</td>`).join('')}
         </tr>
-        `
-    });
+    `).join('')}
 
-    userTable += `
+    <tbody>
     </tbody>
     </table>
     `
 
+    tableDiv.innerHTML = dataTable;
 
-    //Aquí comienza la tabla de login
-    let userLogin = `
-    <table id="tableLogin">
-        <h1>Tabla de login</h1>
-        <thead>
-        <tr>
-            <th>Login</th>
-            <th>Email</th>
-            <th>Password</th>
-            <th>ForgetPassword</th>
-            <th>Signup</th>
-        </tr>
-    </thead>
-    <tbody>
-    `
-
-    loginElement.forEach(element => {
-        userLogin += `
-        <tr>
-            <td>${element.login}</td>
-            <td>${element.email}</td>
-            <td>${element.password}</td>
-            <td>${element.forgetPassword}</td>
-            <td>${element.signup}</td>
-        </tr>
-        `
+    let tableData = new DataTable('#dataTable',{
     });
-
-    userLogin += `
-    </tbody>
-    </table>
-    `
-    //Aquí comienza la tabla de la página principal
-
-    let userIndex = `
-    <table id="tableIndex">
-        <h1>Tabla de información del index</h1>
-        <thead>
-        <tr>
-            <th>Title</th>
-            <th>Image</th>
-            <th>Descripcion</th>
-            <th>Link</th>
-            <th>Clase</th>
-        </tr>
-    </thead>
-    <tbody>
-    `
-
-    indexElement.forEach(element => {
-        userIndex += `
-        <tr>
-            <td>${element.title}</td>
-            <td>${element.image}</td>
-            <td>${element.descripcion}</td>
-            <td>${element.link}</td>
-            <td>${element.clase}</td>
-        </tr>
-        `
-    });
-
-    userIndex += `
-    </tbody>
-    </table>
-    `
-
-    //Aquí comienza la tabla de diseño retro
-    let userDis = `
-    <table id="tableDis">
-        <h1>Tabla de información del index</h1>
-        <thead>
-        <tr>
-            <th>id</th>
-            <th>Título</th>
-            <th>Explicación</th>
-            <th>Título juego</th>
-            <th>Subtítulo</th>
-            <th>Descripción juego</th>
-            <th>Imagen en formato JPG</th>
-        </tr>
-    </thead>
-    <tbody>
-    `
-
-    disElement.forEach(element => {
-        userDis += `
-        <tr>
-            <td>${element.id}</td>
-            <td>${element.titulo}</td>
-            <td>${element.explicacion}</td>
-            <td>${element.tituloJuego}</td>
-            <td>${element.subtitulo}</td>
-            <td>${element.descripcionJuego}</td>
-            <td>${element.imagen}.jpg</td>
-        </tr>
-        `
-    });
-
-    userDis += `
-    </tbody>
-    </table>
-    `
-
-    user.innerHTML = userTable;//Con esto se imprimirá en la vista de la página los datos de la entidad usuario
-    user.innerHTML += userLogin;//Añadirá a parte la tabla del login
-    user.innerHTML += userIndex;//Añadirá a parte la tabla de información del index
-    user.innerHTML += userDis;//Añadirá a parte la tabla de información de diseño retro
-
-    let tableUser = new DataTable('#tableUser',{
-    });//Esto permite cargar el data table de la tabla de usuarios
-    let tableLogin = new DataTable('#tableLogin',{
-    });//Esto permite cargar el data table de la tabla de login
-    let tableIndex = new DataTable('#tableIndex',{
-    });//Esto permite cargar el data table de la tabla del index
-    let tableDis = new DataTable('#tableDis',{
-    });
-
 
 }
 
